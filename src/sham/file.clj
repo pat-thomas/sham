f06e91adb4e033654f2cc4e6b931d9815c689da3
Pat Thomas <pthomas@relaynetwork.com>
2015-01-06 13:25:14 -0500

    Code automatically pulls out schemas from file.

--- src/sham/file.clj
+++ src/sham/file.clj
@@ -9,15 +9,30 @@
   (for [i (range (count rows))]
     (assoc (nth rows i) :id i)))
 
+(defn file-data->tables
+  [file-data]
+  (->> file-data
+       keys
+       (reduce (fn [accum k]
+                 (conj accum [k (-> file-data k keys)]))
+               [])
+       (map (fn [[table schemas]]
+              (reduce (fn [accum schema]
+                        (conj accum [table schema]))
+                      []
+                      schemas)))
+       flatten
+       (partition 2)))
+
 (defn validate!
   []
   (let [parsed-file (cheshire/parse-stream (clojure.java.io/reader "./resources/ws_responses.json") keyword)
         data-rows   (dissoc parsed-file :tables)]
-    (reduce (fn [accum table-and-schema]
-              (let [[table schema] (map keyword (clojure.string/split table-and-schema #"\."))]
-                (update-in accum [table schema] assign-ids)))
+    (reduce (fn [accum [table schema]]
+              (update-in accum [table schema] assign-ids))
             data-rows
-            (:tables parsed-file))))
+            (file-data->tables data-rows))))
+
 
 (defn load-responses!
   "File should be located at ./resources/ws_responses.json"
